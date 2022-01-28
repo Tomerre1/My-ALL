f36d3b55631b82fef412b4f79ef46e4e7aa16876
Tomer Revah <revahtomer@gmail.com>
2022-01-29 01:59:13 +0200

    mail in navbar bug

--- src/cmps/Header/Nav.jsx
+++ src/cmps/Header/Nav.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 export const Nav = ({ open, toggleOpen, user, onLogout }) => {
-  console.log('%c  user:', 'color: white;background: red;', user);
+  const mail = user ? user.mail : null
   return (
     <ul className={`right-nav ${open ? 'open' : ''}`}>
       <NavLink onClick={toggleOpen} exact to="/">בית</NavLink>
       {user && user.userType === 'מטופל' && <NavLink onClick={toggleOpen} to="/timeline">מסלול</NavLink>}
       {<NavLink to="/medicines">תרופות</NavLink>}
       {user && user.userType === 'אדמין' && <NavLink onClick={toggleOpen} to="/admin">טבלת תרופות</NavLink>}
-      {!user.mail && <NavLink onClick={toggleOpen} to="/auth">התחברות</NavLink>}
-      {user.mail && <NavLink onClick={onLogout} to="/auth">התנתקות</NavLink>}
+      {user && !mail && <NavLink onClick={toggleOpen} to="/auth">התחברות</NavLink>}
+      {user && mail && <NavLink onClick={onLogout} to="/auth">התנתקות</NavLink>}
     </ul>
   )
 }
