e62d58141c1b9ced7436f14a782bef3c9a44ab7a
Tomer Revah <revahtomer@gmail.com>
2022-02-11 17:22:00 +0200

    fixing cards preview

--- src/cmps/SuccessStories/SuccessStoryPreview.jsx
+++ src/cmps/SuccessStories/SuccessStoryPreview.jsx
@@ -7,24 +7,25 @@ import DeleteIcon from '@mui/icons-material/Delete';
 export function SuccessStoryPreview({ story, removeToyReview, user }) {
     console.log('%c  story:', 'color: white;background: red;', story);
     console.log('%c  user:', 'color: white;background: red;', user);
+    const isAdminOrUserWrote = (user && ((user.mail === story.user.mail) || user.userType === 'אדמין'))
     return (
         <section className="success-story">
-            {(user && ((user.mail === story.user.mail) || user.userType === 'אדמין')) &&
-                <div className="success-story-header">
-                    <Avatar style={{ backgroundColor: '#ff7518' }}>{story.user.fullname[0].toUpperCase()}</Avatar>
+            <div className="success-story-header" style={{ justifyContent: (isAdminOrUserWrote) ? 'space-between' : 'flex-end' }}>
+                {isAdminOrUserWrote &&
                     <div className="actions">
                         <button onClick={() => { console.log('edit') }} ><EditIcon /></button>
                         <button onClick={() => { console.log('delete') }} ><DeleteIcon /></button>
                     </div>
-                </div>
-            }
+                }
+                <Avatar style={{ backgroundColor: '#ff7518' }}>{story.user.fullname[0]}</Avatar>
+            </div >
 
             <div className="success-story-comment">
-                <p className="title">{'כותרת'}</p>
-                <p className="content"> {'בלה בלה בלה'}</p>
+                <p className="title">{story.title}</p>
+                <p className="content"> {story.content}</p>
             </div>
 
-            <span>{utilService.makeDateWithHour(story.date)}</span>
+            <span className="date">{utilService.makeDateWithHour(story.date)}</span>
         </section >
     )
 }
--- src/pages/SuccessStories.jsx
+++ src/pages/SuccessStories.jsx
@@ -4,24 +4,17 @@ import { SuccessStoriesList } from '../cmps/SuccessStories/SuccessStoriesList.js
 // import { loadReviews } from '../store/review.actions'
 
 export function SuccessStories() {
-    // async componentDidMount() {
-    //     await this.props.loadReviews({})
-    // }
+
     const user = useSelector(state => state.userReducer.user)
-    const stories = [{ user: { fullname: 'תומר רווח', userType: 'מטופל', mail: 'revahtomer@gmail.com' }, title: 'כותרת', comment: 'תגובה תגובה תגובה', date: new Date() }]
+    const stories = [{ user: { fullname: 'תומר רווח', userType: 'מטופל', mail: 'revahtomer@gmail.com' }, title: 'כותרת', content: 'תגובה תגובה תגובה', date: new Date() }]
     return (
         <div className="success-stories-layout">
             <div className="header">
                 <div className="name">
-                    <p>סיפורי הצלחה</p>
+                    <h1>סיפורי הצלחה</h1>
                 </div>
             </div>
             <hr className="border" />
-            {/* <div className="card__insights">
-                <div className="card__heading">
-                    <div className="heading">Store Reviews</div>
-                </div>
-            </div> */}
             {stories.length > 0 && <div className="success-stories-container">
                 <SuccessStoriesList stories={stories} user={user} />
             </div>
