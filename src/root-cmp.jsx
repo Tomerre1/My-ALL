f5cac5f0dc3c91a8e838818bb067d0977030b38b
Tomer Revah <revahtomer@gmail.com>
2022-04-13 11:49:50 +0300

    Accessibility plugin

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router'
 import routes from './routes'
 import { AppHeader } from './cmps/Header/AppHeader'
+import { Accessibility } from 'accessibility/src/main';
+
 
 export function RootCmp() {
-    console.log('%c  window.location.pathname:', 'color: white;background: red;', window.location.pathname);
+    useEffect(() => {
+        var labels = {
+            resetTitle: 'איפוס הגדרות',
+            closeTitle: 'סגירת מסך נגישות',
+            menuTitle: 'מסך נגישות',
+            increaseText: 'הגדלת טקסט',
+            decreaseText: 'הקטנת טקסט',
+            increaseTextSpacing: 'הגדלת ריווח טקסט',
+            decreaseTextSpacing: 'הקטנת ריווח טקסט',
+            invertColors: 'היפוך צבעים',
+            grayHues: 'צבעי אפור',
+            underlineLinks: 'קו תחתי לינקים',
+            bigCursor: 'סמן גדול',
+            readingGuide: 'מדריך קריאה',
+            textToSpeech: 'טקסט לדיבור',
+            speechToText: 'הקראת טקסט',
+        };
+        var options = { labels: labels };
+        options.textToSpeechLang = 'he'; // or any other language
+        options.speechToTextLang = 'he'; // or any other language
+        window.addEventListener('load', function () { new Accessibility(options); }, false);
+        return () => {
+            window.removeEventListener('load', function () { new Accessibility(options); }, false);
+        }
+    }, [])
+
 
     return (
         <div>
